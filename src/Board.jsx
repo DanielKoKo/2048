6c0da803f6187dd3bfb5b75e45aaaee7157be9e9
DanielKoKo <danielko1202@gmail.com>
2024-10-17 12:58:13 -0700

    Board generates the 2 initial tiles upon startup

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,15 +1,52 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, memo} from 'react';
 import './Board.css';
 
 function Board() {
     const [tiles, setTiles] = useState(Array(16).fill(''));
-    
+    const [initialized, setInitialized] = useState(false);
+
     let colors = {
         '2': '#F0E4D9',
         '4': '#EFE0C5',
         '8': '#FCAE6F',
         '16': '#FF9057',
         '32': '#FF7456'
+    }
+    
+    useEffect(() => {
+        if (!initialized) {
+            placeRandomTile('2');
+            placeRandomTile('2');
+            setInitialized(true);
+        }
+
+        window.addEventListener('keydown', moveTiles);
+
+        return () => {
+            window.removeEventListener('keydown', moveTiles);
+        }
+    }, []);
+
+    function placeRandomTile(val) {
+        // arrow function ensures that we're working with the most current state
+        // fixes issue where only 1 tile is generated upon startup
+        setTiles((prevTiles) => {
+            let newTile;
+
+            do {
+                newTile = Math.floor(Math.random() * 17);
+            } while (prevTiles[newTile] !== '');
+    
+            const newTiles = [...prevTiles];
+            newTiles[newTile] = val;
+            return newTiles;
+        })
+    }
+
+    function moveTiles(e) {
+        const direction = e.key;
+
+        
     }
 
     function renderTile(i) {
