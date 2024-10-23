6cd054ad8ec06bdbadc8043a607184a8ee50cfc2
DanielKoKo <danielko1202@gmail.com>
2024-10-23 11:26:25 -0700

    Added useEffect import for Score.jsx

--- src/Score.jsx
+++ src/Score.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import { useCookies } from 'react-cookie';
 import { GameContext } from './App';
 import './Score.css';
